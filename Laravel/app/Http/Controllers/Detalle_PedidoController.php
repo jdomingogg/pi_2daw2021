79d4e4d87c415ee69f18355e6f5dc3d211ea2492
jdomingogg <jdgarridogalve@gmail.com>
2021-05-05 01:14:27 +0200

    controladores creados

---
+++ Laravel/app/Http/Controllers/Detalle_PedidoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detalle_Pedido;
+use Illuminate\Http\Request;
+
+class Detalle_PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Detalle_Pedido  $detalle_Pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Detalle_Pedido $detalle_Pedido)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Detalle_Pedido  $detalle_Pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Detalle_Pedido $detalle_Pedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Detalle_Pedido  $detalle_Pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Detalle_Pedido $detalle_Pedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Detalle_Pedido  $detalle_Pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Detalle_Pedido $detalle_Pedido)
+    {
+        //
+    }
+}
---
+++ Laravel/app/Http/Controllers/Lista_DeseosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lista_Deseos;
+use Illuminate\Http\Request;
+
+class Lista_DeseosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lista_Deseos  $lista_Deseos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lista_Deseos $lista_Deseos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lista_Deseos  $lista_Deseos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lista_Deseos $lista_Deseos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lista_Deseos  $lista_Deseos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lista_Deseos $lista_Deseos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lista_Deseos  $lista_Deseos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lista_Deseos $lista_Deseos)
+    {
+        //
+    }
+}
